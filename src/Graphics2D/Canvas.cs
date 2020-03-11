1b0d67799a8fafc153e9b89d33a83b7064982207
AbnerSquared <25711694+AbnerSquared@users.noreply.github.com>
2020-03-11 13:40:46 -0500

    File transfer and refactor Animating: v0.0.14
    - Transferred all existing files into "src/".
    - Completely refactored all of Orikivo.Drawing.Animating.

---
+++ src/Graphics2D/Canvas.cs
@@ -0,0 +1,18 @@
+﻿namespace Orikivo.Drawing.Graphics2D
+{
+    public class Canvas
+    {
+        // individual pixels.
+        public Grid<System.Drawing.Color> Pixels { get; set; }
+        public int Width { get; }
+        public int Height { get; }
+        public Pen Pen { get; set; }
+
+        public void PenDown() { }
+        public void PenUp() { }
+        public void Clear() { }
+        
+        public void Stamp(System.Drawing.Image image)
+        { }
+    }
+}
---
+++ src/Graphics2D/Pen.cs
@@ -0,0 +1,9 @@
+﻿namespace Orikivo.Drawing.Graphics2D
+{
+    public class Pen
+    {
+        public System.Drawing.Color Color { get; set; }
+        public float Size { get; set; }
+        public bool IsDown { get; set; }
+    }
+}
